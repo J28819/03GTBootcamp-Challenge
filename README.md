c2a3b3d22cc4737c692614a99386f5bbe4e8c638
BC_IML <lelj28819@gmail.com>
2022-05-16 22:25:59 -0400

    Git Folder was renamed as recommendation for the last project review for unique name

--- README.md
+++ README.md
@@ -12,17 +12,23 @@ Index.html -> Contain all the code that will be processes by the browser to disp
 1. To clone the repository in your computer run this command.
 
 ```bash
-git clone git@github.com:J28819/03GTBootcamp-Challenge.git
+git clone git@github.com:J28819/03PassGenIML.git
 
 ```
 
 2. Then open it with Visual Code and run Live Server on the [Index.html file.]
 
+3. You can Follow this link : 
+
 
 
 ## Description
-This first Challenge was made in Visual code modifying the existing code on the Index.html and Style.css, reducing the clases for similar objects and clean the code as much as I can. 
+This Password Generator has a catalog of what type of diferent data they could add in the way the system can be expanded in the future for more charachters or lenguages.
 
+The user have to press the button "Generate Password" then select the number of characters desired between 8 and 128, after tnat they will receive different confirms to add different types of data such as Numbers, LowerCase Letters, Upper Case Leters or Symbols that are in the actual catalog stored in the variable ##confirmsMsg. 
+
+If the user have more than one type of data selected at the end they will have the password with those attributes and ## "Well Done" or 
+"Try Again" that will be changed in the next 3 seconds to shows "Generate a new Password" or  "Generate Password" 
 ## Screenshoots
 
 ![My animated logo](assets/readme-pics/overview.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // Globals
 var generateBtn = document.querySelector("#generate");
+
 let alternativemsg = "Becasue Security is important please try again using one of this options Numbers, LowerCase Letters, UpperCase Letters or Symbols and protect your information and equipment."
 
 // for future this will be good to add an admin page and make changes the configuration server side for new symbols, different lenguages,  kanji signs etc...
 // Declare Objects to create an easy way to add or remove chain of different characters
-var confirmsMsg = [{
+let confirmsMsg = [{
                 "message": "Do you want to add Numbers" ,
                 "DataType": "0123456789"
               },
@@ -22,95 +23,71 @@ var confirmsMsg = [{
               }
             ]
 
+            function generatePassword(passwordResult, passLenght) {
+              //generateBtn.innerHTML = "Password Ready!"
+              var GenPassword = ""
+              var mixerpass = ""
+              var maxchar = passwordResult.length;
+              for ( var ii = 0; ii < passLenght; ii++ ) {
+                    var random = Math.floor(Math.random() * maxchar);
+                    GenPassword = GenPassword + passwordResult.charAt(random);
+                   }
+                   //Mix the charachters for doubble unique sequence.
+                   mixerpass = GenPassword.split('').sort(function(){return 0.5-Math.random()}).join('');
+                  // return password to be written into the front end
+                    return(mixerpass);
+                  
+                  
+                }
+                
+
 // this function will lead the prompts in base of the configuration objects in Prompts 
 function writePassword() {
-
-  function generatePassword(passwordResult, passLenght) {
-    //generateBtn.innerHTML = "Password Ready!"
-    var GenPassword = ""
-    var mixerpass = ""
-    
-      //var charachterstochose = passwordResult;
-      console.log("this are the charachters to chose: " + passwordResult)
-      console.log("this is the numbers of charachters: " + passLenght)
-    
-    
-      if (passwordResult === "") {
-        var nothingtyped = confirm('Please select minimum one of the different types of charachters, \nDo you wanto to try again?')
-        if (nothingtyped){
-          writePassword()
-        }else {
-         mixerpass = alternativemsg
-         return(mixerpass)
-          
-        }
-        }else {
-          var maxchar = passwordResult.length;
-          for ( var ii = 0; ii < passLenght; ii++ ) {
-            var random = Math.floor(Math.random() * maxchar);
-            GenPassword = GenPassword + passwordResult.charAt(random);
-          
-              
-         }
-         console.log(GenPassword);
-         console.log(typeof(GenPassword));
-         //Mix the charachters for doubble unique sequence.
-         mixerpass = GenPassword.split('').sort(function(){return 0.5-Math.random()}).join('');
-          
-        // return password to be written into the front end
-          return(mixerpass);
-        }
-        
-      }
-      
-  
   var passwordResult = "";
-  
   var passLenght = prompt("Please write the number of charachters in your password between 8 and 128");
-  
-  console.log()
-  if (passLenght > 8 && passLenght < 128) {
-    
+
+  if (passLenght >= 8 && passLenght <= 128 ) {
       confirmsMsg.forEach(element => { 
         var confirmResult = confirm(element.message)
         if (confirmResult) {
           passwordResult += element.DataType
-          
         }   
       }
       );
-
-   
-      
    }else {
     alert("Please write a valid number between 8 and 128");
     writePassword()
     }
 
-  
+    if (passwordResult === "") {
+      var nothingtyped = confirm('Please select minimum one of the different types of charachters, \nDo you wanto to try again?')
+      if (nothingtyped){
+        return
+      }else {
+        generateBtn.innerHTML = "Try again if you change your mind..."
+        var tryagainlater = document.querySelector("#password")
+        tryagainlater.value = "Try Again!!";
+        setTimeout(function(){
+          window.location.reload()
+        }, 1000)
+        return
+      }
+    }
 
   // call generatePassword function and passing arguments for password structure
   var password = generatePassword(passwordResult, passLenght);
   var passwordText = document.querySelector("#password");
-  
   passwordText.value = password;
-  if (password !== alternativemsg) {
-    generateBtn.innerHTML = "Well Done!"
+ 
+  if (password) {
+    generateBtn.innerHTML = "Well Done! Thanks to Secure your Password."
     setTimeout(function(){
       generateBtn.innerHTML = "Generate a new Password"
       
-    }, 3000)
-  }else {
-    generateBtn.innerHTML = "Try Again!!"
-    setTimeout(function(){
-      generateBtn.innerHTML = "Generate Password"
-      
-    }, 3000)
+    }, 1500)
   }
-  
 }
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);document.getElementById("generate").innerHTML = "Processing..."
 generateBtn.addEventListener("click",  writePassword)
 
